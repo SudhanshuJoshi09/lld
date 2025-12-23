d1205f87113369603a01aa7c7440f866d37c6864
SudhanshuJoshi <sudhanshujoshi49@gmail.com>
2025-12-23 11:25:34 +0530

    chore: added scheduling with add task functionality

--- jobscheduler/main.go
+++ jobscheduler/main.go
@@ -8,72 +8,136 @@ import (
 	"context"
 )
 
+type Scheduler struct {
+	tasks []Task
+	add chan Task
+
+	ctx context.Context
+	cancel context.CancelFunc
+}
+
+
+func NewScheduler(tasks []Task) *Scheduler {
+	add := make(chan Task)
+	ctx, cancel := context.WithCancel(context.Background())
+
+	return &Scheduler{
+		tasks: tasks,
+		add: add,
+
+		ctx: ctx,
+		cancel: cancel,
+	}
+}
+
+
 type Task struct {
 	name string
 	action func()
 	scheduledTime time.Time
 }
 
-func runJobs(ctx context.Context, tasks []Task) {
-	now := time.Now()
-	for i := 0; i < len(tasks); i++ {
-		wait := tasks[i].scheduledTime.Sub(now)
-
-		if wait > 0 {
+func (s *Scheduler) runJobs() {
+	for true {
+		if len(s.tasks) == 0 {
 			select {
-			case <- time.After(wait):
-			case <- ctx.Done():
+			case <- s.ctx.Done():
+				fmt.Println("Cancel event triggered")
 				return
+			case v, ok := <- s.add:
+				if !ok {
+					fmt.Println("channel closed -> done")
+					return
+				}
+				s.tasks = append(s.tasks, v)
+				continue
 			}
 		}
 
-		tasks[i].action()
-		now = tasks[i].scheduledTime
+		select {
+		case <- time.After(time.Until(s.tasks[0].scheduledTime)):
+			s.tasks[0].action()
+			s.tasks = s.tasks[1:]
+		case <- s.ctx.Done():
+			return
+		case v, ok := <- s.add:
+			if !ok {
+				fmt.Println("done")
+				return
+			}
+			s.tasks = append(s.tasks, v)
+			s.orderTasks()
+			continue
+		}
 	}
 }
 
-func cancelJobs(cancel context.CancelFunc) {
-	cancel()
+func (s *Scheduler) addTask(task Task) {
+	s.add <- task
+}
+
+func (s *Scheduler) cancelJobs() {
+	s.cancel()
 }
 
 
-func orderTasks(tasks []Task) []Task {
-	sort.Slice(tasks, func(i, j int) bool {
-		return tasks[i].scheduledTime.Before(tasks[j].scheduledTime)
+func (s *Scheduler) orderTasks() {
+	sort.Slice(s.tasks, func(i, j int) bool {
+		return s.tasks[i].scheduledTime.Before(s.tasks[j].scheduledTime)
 	})
-	return tasks
 }
 
 func main() {
 	var wg sync.WaitGroup
+	now := time.Now()
 	tasks := []Task{
 		{
 			name: "Task - 01",
-			scheduledTime: time.Now().Add(time.Second * 4),
+			scheduledTime: now.Add(time.Second * 4),
 			action: func() { fmt.Println("job 1 executed") },
 		},
 		{
 			name: "Task - 02",
-			scheduledTime: time.Now().Add(time.Second * 8),
+			scheduledTime: now.Add(time.Second * 8),
 			action: func() { fmt.Println("job 2 executed") },
 		},
 	}
-	tasks = orderTasks(tasks)
-
-	ctx, cancel := context.WithCancel(context.Background())
-
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		runJobs(ctx, tasks)
-	}()
-	time.Sleep(time.Second * 6)
+	s := NewScheduler(tasks)
+	s.orderTasks()
 
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		cancelJobs(cancel)
+		for i := 0; i < 10; i++ {
+			fmt.Println(time.Now())
+			time.Sleep(1 * time.Second)
+		}
 	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		s.runJobs()
+	}()
+	time.Sleep(time.Second * 1)
+
+	newTask := Task{
+		name: "Task - 03",
+		scheduledTime: now.Add(time.Second * 3),
+		action: func() { fmt.Println("job 3 executed") },
+	}
+
+	s.addTask(newTask)
+
+	time.Sleep(time.Second * 8)
+
+	close(s.add)
+
+	// wg.Add(1)
+	// go func() {
+	// 	defer wg.Done()
+	// 	s.cancelJobs()
+	// }()
 
 	wg.Wait()
 }
