9280a8b0bc0f6b7c41168d9aeb032d45cf3c70a0
SudhanshuJoshi <sudhanshujoshi49@gmail.com>
2025-12-31 20:06:30 +0530

    chore: added state conversion TODO -> DONE

---
+++ basics/todolist/README.md
@@ -0,0 +1,9 @@
+## TODO-List Generator
+
+- Features
+1. Create a task with title and description.
+2. Get all the task from a file (tasks.json).
+
+APIs Exposed:
+1. put(taskList []Task) -> error
+2. get() -> []Task, error
--- basics/todolist/main.go
+++ basics/todolist/main.go
@@ -1,57 +1,110 @@
 package main
 
-
 import (
-	"os"
-	"fmt"
+	// "bufio"
 	"encoding/json"
+	"fmt"
+	"os"
 )
 
-const fileName = ".output/content.txt"
+const fileName = ".output/tasks.json"
+
+
+const (
+	TODO TaskStatus = iota
+	DONE
+)
+
+type TaskStatus int
+
+
+type transition struct {
+	from TaskStatus
+	to   TaskStatus
+}
+
+var validTransitions = map[transition]struct{}{
+	{TODO, DONE}:   {},
+}
+
+func validStateTransition(from, to TaskStatus) bool {
+	_, ok := validTransitions[transition{from, to}]
+	return ok
+}
+
 
 type Task struct {
 	Title string `json:"name"`
 	Description string `json:"description"`
-	Done bool `json:"done"`
+	TaskStatus TaskStatus `json:"task_status"`
 }
 
-func put(taskList []Task) {
+
+func NewTask(title, description string) Task {
+	return Task {
+		Title: title,
+		Description: description,
+		TaskStatus: TODO,
+	}
+}
+
+func put(taskList []Task) error {
 	content, err := json.Marshal(taskList)
 	if err != nil {
-		panic(err)
+		return err
 	}
 	err = os.WriteFile(fileName, content, 0667)
 	if err != nil {
-		panic(err)
+		return err
 	}
+	return nil
 }
 
-func get() []Task {
+func get() ([]Task, error) {
 	content, err := os.ReadFile(fileName)
 	if err != nil {
-		panic(err)
+		return []Task{}, err
 	}
 	var task []Task
 	err = json.Unmarshal(content, &task)
 	if err != nil {
-		panic(err)
+		return []Task{}, err
 	}
-	return task
+	return task, nil
+}
+
+func (t *Task) updateStatus(to TaskStatus) error {
+	fmt.Println(t.TaskStatus, to)
+	if validStateTransition(t.TaskStatus, to) {
+		t.TaskStatus = to
+		return nil
+	} else {
+		return fmt.Errorf("failed to update status : invalid status change")
+	}
+}
+
+func readTask() Task {
+	var title string
+	var description string
+	fmt.Scanln(&title)
+	fmt.Scanln(&description)
+	return Task {Title: title, Description: description}
 }
 
 func main() {
 	// Adding precursor condition of loading the list from the memory and then appending items.
-	taskList := get()
-	task := Task{Title: "Something3", Description: "Third ever task"}
-	taskList = append(taskList, task)
-
+	taskList, _ := get()
+	err := taskList[0].updateStatus(DONE)
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = taskList[0].updateStatus(TODO)
+	if err != nil {
+		fmt.Println(err)
+	}
 	put(taskList)
-	actualTaskList := get()
 
-	actualTaskList[0].Done = true
-	put(actualTaskList)
+	actualTaskList, _ := get()
 
-	newActualTaskList := get()
-
-	fmt.Println(newActualTaskList)
+	fmt.Println(actualTaskList)
 }
