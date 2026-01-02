6b9c307aa25ef68ec115e39e9e8836bf8dcae114
SudhanshuJoshi <sudhanshujoshi49@gmail.com>
2026-01-02 11:19:46 +0530

    chore: Added few small projects

---
+++ basics/kvstore/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"encoding/json"
+	"time"
+)
+
+type Record struct {
+	Key string `json:"key"`
+	Value string `json:"value"`
+	ExpireAt time.Time `json:"expire_at"`
+}
+
+const fileName = ".output/kvstore.json"
+
+
+type KVStore struct  {
+	mapping map[string]Record
+}
+
+func NewKVStore() *KVStore {
+	return &KVStore{
+		mapping: make(map[string]Record),
+	}
+}
+
+
+func (kv *KVStore) get(key string) (string, error) {
+	currEntry, ok := kv.mapping[key]
+	now := time.Now()
+	if !ok {
+		return "", fmt.Errorf("Key not found %v", key)
+	}
+	if currEntry.ExpireAt.Before(now) {
+		delete(kv.mapping, key)
+		kv.writeRecords()
+		return "", fmt.Errorf("Key not found %v", key)
+	}
+	return currEntry.Value, nil
+}
+
+func (kv *KVStore) set(key, value string, ttl time.Duration) {
+	entry := Record { Key: key, Value: value, ExpireAt: time.Now().Add(ttl) }
+	kv.mapping[key] = entry
+	kv.writeRecords()
+}
+
+func (kv *KVStore) delete(key string) error {
+	_, ok := kv.mapping[key]
+	if !ok {
+		return fmt.Errorf("Key not found %v", key)
+	}
+	delete(kv.mapping, key)
+	kv.writeRecords()
+	return nil
+}
+
+func (kv *KVStore) writeRecords() {
+	byteContent, _ := json.Marshal(kv.mapping)
+	os.WriteFile(fileName, byteContent, 0644)
+}
+
+func (kv *KVStore) readRecords() map[string]Record {
+	content, _ := os.ReadFile(fileName)
+	var result map[string]Record
+	json.Unmarshal(content, &result)
+	return result
+}
+
+
+func main() {
+	kvstore := NewKVStore()
+	kvstore.set("Sudhanshu", "Joshi", time.Second * 2)
+	time.Sleep(4 * time.Second)
+	result, err := kvstore.get("Sudhanshu")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(result)
+}
